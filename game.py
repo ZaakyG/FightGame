2fb796854dfcc4c19ac2c9dcace75faf3bcf8859
Isaac G <urisac.gc@hotmail.com>
2022-11-07 15:12:47 -0600

    Methods name definition

--- game.py
+++ game.py
@@ -5,4 +5,15 @@ class Game():
     Actual game class
     '''
     def __init__(self, player_name):
-        self.character = character.Character()
+        self.character = character.Character(player_name)
+
+    def main_game(self):
+        pass
+
+    def battle(self):
+        pass
+
+    def use_energy(self):
+        pass
+
+    
